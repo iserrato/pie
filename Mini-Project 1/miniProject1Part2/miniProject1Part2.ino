4c3ace7ff08d255f34e85b0081d1f98fc0e9293d
Isabel Serrato <iserrato@olin.edu>
2021-09-09 10:52:04 -0400

    Complete part 2 and edit comments in part 1

--- Mini-Project 1/miniProject1Part2/miniProject1Part2.ino
+++ Mini-Project 1/miniProject1Part2/miniProject1Part2.ino
@@ -1,8 +1,9 @@
 /*
   Project 1 Code
 
-  Part 1 of Mini-Project 1: Build Your Own Bike Light
-  Turns three LEDs on/off in different patterns depending on the mode, which can be altered using the switch
+  Part 2 of Mini-Project 1: Build Your Own Bike Light
+  Uses an infrared distance senseor to modulate the delay between our lights turning on/off.
+  
 */
 
 // define each of the pins we are using for the LEDs
@@ -11,8 +12,14 @@ int LED2 = 11;
 int LED3 = 10;
 int mode = 0;
 
-// initialize start time
 unsigned long myTime;
+
+// define pin for IR sensor
+int sharpIR = A0; 
+
+// initialize starting values for our sensor and delay
+int sensorValue = 0;
+int dVal = 500;
 
 void setup() 
 {
@@ -20,7 +27,6 @@ void setup()
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
-  
   // initialize digital pin as an input that is held high
   pinMode(2, INPUT_PULLUP);
 
@@ -34,70 +40,79 @@ void setup()
 }
 
 void loop() {
+  sensorValue = analogRead(sharpIR);
+  Serial.println(sensorValue);
   // LED behavior based on mode
+  dVal = 650 - sensorValue;
+  
   switch(mode) {
     case 0: // all on
       digitalWrite(LED1, HIGH);
       digitalWrite(LED2, HIGH);
       digitalWrite(LED3, HIGH);
-      break;
-    case 1: // all off
+      delay(dVal);
       digitalWrite(LED1, LOW);
       digitalWrite(LED2, LOW);
       digitalWrite(LED3, LOW);
+      delay(dVal);
       break;
-    case 2: // sequential on 
+    case 1: // sequential on 
       digitalWrite(LED1, HIGH);
-      delay(100); 
+      delay(dVal); 
       digitalWrite(LED2, HIGH);
-      delay(100);
+      delay(dVal);
       digitalWrite(LED3, HIGH);
-      delay(100);
+      delay(dVal);
       digitalWrite(LED1, LOW);
-      delay(100); 
+      delay(dVal); 
       digitalWrite(LED2, LOW);
-      delay(100);
+      delay(dVal);
       digitalWrite(LED3, LOW);
-      delay(100);
+      delay(dVal);
       break;
-    case 3: // bounce
+    case 2: // bounce
       digitalWrite(LED1, HIGH);
-      delay(100); 
+      delay(dVal); 
       digitalWrite(LED1, LOW);
-      delay(100);
+      delay(dVal);
       digitalWrite(LED2, HIGH);
-      delay(100);
+      delay(dVal);
       digitalWrite(LED2, LOW);
-      delay(100);
+      delay(dVal);
       digitalWrite(LED3, HIGH);
-      delay(100);
+      delay(dVal);
       digitalWrite(LED3, LOW);
-      delay(100);
+      delay(dVal);
       digitalWrite(LED3, HIGH);
-      delay(100); 
+      delay(dVal); 
       digitalWrite(LED3, LOW);
-      delay(100);
+      delay(dVal);
       digitalWrite(LED2, HIGH);
-      delay(100);
+      delay(dVal);
       digitalWrite(LED2, LOW);
-      delay(100);
+      delay(dVal);
       digitalWrite(LED1, HIGH);
-      delay(100);
+      delay(dVal);
       digitalWrite(LED1, LOW);
-      delay(100);
+      delay(dVal);
       break;
-    case 4: //random on
+    case 3: // random on
       int randint = floor(random(10,13));
       digitalWrite(randint, HIGH);
-      delay(100);
+      delay(dVal);
       digitalWrite(randint, LOW);
-      delay(100);
+      delay(dVal);
+      break;
+    case 4: // all off
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
       break;
   } 
 }
 
 void update_mode() {
-  // Increment mode based on switch
+  // Increment mode based on switch. diff ensures the switch does not happen if the sensor reads a change withing one second of the last change
   unsigned long diff = millis()- myTime;
   if (diff > 1000) {
     mode+=1;
