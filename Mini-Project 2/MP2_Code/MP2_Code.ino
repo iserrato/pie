3a0466f8a21c6ae8c9c5b3a82efad4c88ed031bb
Isabel Serrato <iserrato@olin.edu>
2021-09-19 12:56:57 -0400

    Add send/receive functionality
    Implement arduino code to send angle and distance data, python receives the data, translates to cartesian, and plots

--- Mini-Project 2/MP2_Code/MP2_Code.ino
+++ Mini-Project 2/MP2_Code/MP2_Code.ino
@@ -4,6 +4,10 @@ Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
 int pos = 0;    // variable to store the servo position
+int sharpIR = A0; 
+
+// initialize starting values for our sensor and delay
+int sensorValue = 0;
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
@@ -18,9 +22,17 @@ void loop() {
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
+    sensorValue = analogRead(sharpIR);
+    Serial.print(pos);  Serial.print(",");
+    Serial.println(sensorValue);
+    //delay(400);
   }
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
+    sensorValue = analogRead(sharpIR);
+    Serial.print(pos);  Serial.print(",");
+    Serial.println(sensorValue);
+    //delay(400);
   }
 }
--- Mini-Project 2/PythonReceiveData.py
+++ Mini-Project 2/PythonReceiveData.py
@@ -8,6 +8,9 @@
 
 
 import serial
+import plotly.graph_objects as go
+import pandas as pd
+import math
 
 #
 # Note 1: This python script was designed to run with Python 3.
@@ -30,7 +33,7 @@ import serial
 # For Windows computers, the name is formatted like: "COM6"
 # For Apple computers, the name is formatted like: "/dev/tty.usbmodemfa141"
 #
-arduinoComPort = "COM4"
+arduinoComPort = "COM1"
 
 
 #
@@ -47,7 +50,8 @@ baudRate = 9600
 #
 serialPort = serial.Serial(arduinoComPort, baudRate, timeout=1)
 
-
+x_vals = []
+y_vals = []
 
 #
 # main loop to read data from the Arduino, then display it
@@ -66,12 +70,30 @@ while True:
     #
     # data was received, convert it into 4 integers
     #
-    a, b, c, d = (int(x) for x in lineOfData.split(','))
+    print("")
+    angle, val = (int(x) for x in lineOfData.split(','))
 
     #
     # print the results
     #
-    print("a = " + str(a), end="")
-    print(", b = " + str(b), end="")
-    print(", c = " + str(c), end="")
-    print(", d = " + str(d))
+
+    print("angle = " + str(angle), end="")
+    print(", val = " + str(val), end="")
+
+    distance = -(val - 716.079)/10.4921
+    theta = angle * math.pi/180.0
+
+    cart_x = distance * math.cos(theta - 90)
+    cart_y = distance * math.sin(theta - 90)
+
+    x_vals.append(cart_x)
+    y_vals.append(cart_y)
+
+    if len(x_vals) > 180:
+        break
+
+print(x_vals[:10])
+print(y_vals[:10])
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=x_vals, y=y_vals))
+fig.show()
